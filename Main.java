ce0a6ee23bb6e9431a06fb800bde5cf3588c986e
Duvvadeepthi148 <119621882+Duvvadeepthi148@users.noreply.github.com>
2022-12-02 00:00:35 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+       eter");
+        String name=input.readLine();
+        System.out.println(nameConsole input=System.console();
+        System.out.println("en);
+    }
+
+}
